e6208513444d73aeb88053db7ea0392d3cefe830
jiezhou81 <jiezhou81@creditease.cn>
2019-08-08 17:07:02 +0800

    react first commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,75 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+function UserGreeting(props) {
+    return <h1>欢迎回来!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>请先注册。</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LogoutButton() {
+    return <button>退出</button>
+}
+
+function LoginButton() {
+    return <button>登陆</button>
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+
+        let button = null;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
+// ReactDOM.render(
+//     <LoginControl />,
+//     document.getElementById('example')
+// );
+import Tabs from './tabs/tabs'
+ReactDOM.render(
+    <Tabs />,
+    document.getElementById('example')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
